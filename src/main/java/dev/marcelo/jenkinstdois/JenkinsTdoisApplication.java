aaefd3c4ffbacf88fd74b0c172293d14996c41ea
marcelo miyoshi <miyoshi_marcelo@hotmail.com>
2021-11-23 09:38:51 -0300

    first commit

---
+++ src/main/java/dev/marcelo/jenkinstdois/JenkinsTdoisApplication.java
@@ -0,0 +1,13 @@
+package dev.marcelo.jenkinstdois;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTdoisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsTdoisApplication.class, args);
+	}
+
+}
